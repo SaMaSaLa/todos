262eda21569bb242960608821d550cc516421e3f
Sai Vanga <Sai@Sai.Rocks>
2021-07-31 15:28:12 -0700

    Started adding todos

---
+++ scripts.js
@@ -0,0 +1,51 @@
+const addEl = document.getElementById("add");
+const deleteAllEl = document.getElementById("delete-all");
+const divEl = document.getElementById("div");
+
+let j = 1;
+let todos = [];
+
+let pST = localStorage.getItem("todos");
+let pPT = JSON.parse(pST);
+
+if (pPT === null) {
+  pPT = [];
+}
+for (let i = 0; i < pPT.length; i++) {
+  divEl.innerText = "";
+  let pth = document.createElement("p");
+  pth.textContent = pPT[i];
+  divEl.appendChild(pth);
+  pth.style.display = "inline-block";
+
+  let deleteButton2 = document.createElement("button");
+  deleteButton2.textContent = "Delete";
+  divEl.appendChild(deleteButton2);
+}
+
+addEl.addEventListener("click", () => {
+  let todoEl = document.getElementById("added-todo");
+  for (let i = j; i <= j; i++) {
+    let todo = `${i}: ${todoEl.value}`;
+    todos.push(todo);
+    let ptw = document.createElement("p");
+    ptw.textContent = todo;
+    ptw.style.display = "inline-block";
+    divEl.appendChild(ptw);
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    divEl.appendChild(deleteButton);
+    const todosStringed = JSON.stringify(todos);
+    localStorage.setItem("todos", todosStringed);
+    deleteButton.addEventListener("click", function () {
+      let parsed = JSON.parse(localStorage.removeItem("todos"));
+      console.log(parsed);
+    });
+  }
+  j++;
+});
+
+deleteAllEl.addEventListener("click", function () {
+  divEl.innerText = "";
+  localStorage.clear();
+});
