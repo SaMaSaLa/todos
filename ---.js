7992b14bdcff6eb91bd51af14c5ebe1d5326e46c
Lasya Vanga <lasya@lasya.net>
2021-08-02 09:47:05 -0700

    Added Steps

---
+++ ---.js
@@ -0,0 +1,65 @@
+/*
+// // append ol
+// divEl.appendChild(ol);
+
+// let j = 1;
+// let todos = [];
+
+// // get the stringed version
+// let pST = localStorage.getItem("todos");
+
+// // parsed version
+// let pPT = JSON.parse(pST);
+
+// // check for null
+// if (pPT === null) {
+//   pPT = [];
+// }
+
+// for (let i = 0; i < pPT.length; i++) {
+//   // clear everything in the div
+//   divEl.innerText = "";
+
+//   // adding each li
+//   let lit = document.createElement("li");
+//   lit.textContent = pPT[i];
+//   ol.appendChild(lit);
+
+//   // adding the delete button
+//   let deleteButton2 = document.createElement("button");
+//   deleteButton2.textContent = "Delete";
+//   lit.appendChild(deleteButton2);
+// }
+
+// // if you click on the add button
+// addEl.addEventListener("click", () => {
+//   for (let i = j; i <= j; i++) {
+//     // getting element with id added-todo
+//     let todoEl = document.getElementById("added-todo");
+
+//     // adding todo and pushing into array
+//     let todo = `${i}: ${todoEl.value}`;
+//     todos.push(todo);
+
+//     // creating li
+//     let li = document.createElement("li");
+//     li.textContent = todoEl.value;
+//     ol.appendChild(li);
+
+//     // create delete button
+//     let deleteButton = document.createElement("button");
+//     deleteButton.textContent = "Delete";
+//     li.appendChild(deleteButton);
+
+//     // set item into local storage
+//     const todosStringed = JSON.stringify(todos);
+//     localStorage.setItem("todos", todosStringed);
+//   }
+//   j++;
+// });
+
+// deleteAllEl.addEventListener("click", function () {
+//   divEl.innerText = "";
+//   localStorage.clear();
+// });
+*/
--- scripts.js
+++ scripts.js
@@ -1,52 +1,126 @@
-const addEl = document.getElementById("add");
-const deleteAllEl = document.getElementById("delete-all");
-const divEl = document.getElementById("div");
+// const todosContainerEl = document.getElementById("todos-container");
+// const addBtnEl = document.getElementById("add-btn");
+// const delAllBtnEl = document.getElementById("del-all-btn");
+// const txtArea = document.getElementById("txt-area");
 
-let j = 1;
-let todos = [];
+// const olEl = document.createElement("ol");
 
-let pST = localStorage.getItem("todos");
-let pPT = JSON.parse(pST);
+// let todos = [];
 
+// let todosStringed = localStorage.getItem("todos");
+// let todosParsed = JSON.parse(todosStringed);
 
-for (let i = 0; i < pPT; i++) {
-  divEl.innerText = "";
-  let pth = document.createElement("p");
-  pth.textContent = pPT[i];
-  divEl.appendChild(pth);
-  pth.style.display = "inline-block";
+// // append olEl to todosContainerEl
+// todosContainerEl.appendChild(olEl);
 
-  let deleteButton2 = document.createElement("button");
-  deleteButton2.textContent = "Delete";
-  divEl.appendChild(deleteButton2);
-}
+// const renderTodos = (i) => {};
 
-addEl.addEventListener("click", () => {
-  let todoEl = document.getElementById("added-todo");
-  for (let i = j; i <= j; i++) {
-    let todo = `${i}: ${todoEl.value}`;
-    todos.push(todo);
-    let ptw = document.createElement("p");
-    ptw.textContent = todo;
-    ptw.style.display = "inline-block";
-    divEl.appendChild(ptw);
-    let deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    divEl.appendChild(deleteButton);
-    const todosStringed = JSON.stringify(todos);
-    localStorage.setItem("todos", todosStringed);
-    deleteButton.addEventListener("click", function () {
-      let parsed = JSON.parse(localStorage.getItem("todos"));
-      let item = parsed[i - 1];
-      deleteButton.addEventListener("click", () => {
-        localStorage.removeItem(todos[0])
-      });
-    });
-  }
-  j++;
-});
+// const setTodosToLocalStorage = () => {
+//   // parsing peoples todos
+//   let parsedTodos = JSON.stringify(todos);
 
-deleteAllEl.addEventListener("click", function () {
-  divEl.innerText = "";
-  localStorage.clear();
-});
+//   // setting item in local storage
+//   localStorage.setItem("todos", parsedTodos);
+// };
+
+// const renderTodosUI = () => {
+//   // clean everything in ol
+//   olEl.innerText = "";
+
+//   for (let i = 0; i < todos.length; i++) {
+//     // create li
+//   }
+// };
+
+// const addTodosToLocalStorage = () => {
+//   // add li
+//   let li = document.createElement("li");
+
+//   // add text content
+//   li.textContent = txtArea.value;
+
+//   // append to ol
+//   olEl.appendChild(li);
+
+//   // push to peoplesTodos
+//   todos.push(txtArea.value);
+
+//   // set to local storage
+//   setTodosToLocalStorage();
+
+//   // add delete button
+//   const dltBtn = document.createElement("button");
+//   dltBtn.textContent = "Delete";
+
+//   // append delete button
+//   li.appendChild(dltBtn);
+// };
+
+// const deleteTodosfromLocalStorage = () => {};
+
+// const clearTodosFromLocalStorage = () => {
+//   //clear div
+//   todosContainerEl.innerText = "";
+
+//   //clear local storage
+//   localStorage.clear();
+// };
+
+// addBtnEl.addEventListener("click", addTodosToLocalStorage);
+// delAllBtnEl.addEventListener("click", clearTodosFromLocalStorage);
+
+// Only change Local Storage - Don't create/touch/look at UI
+
+const addTodoToLocalStorage = (todo) => {
+  // Test if todo is not empty, not null, not undefined, not empty after trimmed
+  // Get todo string from local storage
+  // Convert todo string to an array
+  // Check if todo array is null
+  // push todo to todo array
+  // convert todo array to todo string
+  // save todo string in local storage
+  // Call render todos ui from local storage here
+};
+
+const deleteTodoFromLocalStorage = (index) => {
+  // Test if index is not null or undefined
+  // Get todo string from local storage
+  // Convert todo string to an array
+  // Check if todo array is null
+  // delete todo at index from todo array
+  // convert todo array to todo string
+  // save todo string in local storage
+  // Call render todos ui from local storage here
+};
+
+const deleteAllTodosFromLocalStorage = () => {
+  // clear everything from local storage
+  // Call render todos ui from local storage here
+};
+
+// Update UI here.
+// Render all the items that we have in Local Storage
+const renderTodosUIFromLocalStorage = () => {
+  // Empty todo elements
+  // Get todo string from local storage
+  // Convert todo string to an array
+  // Check if todo array is null
+  // If todo array has 0 elements: show "No Todos"
+  // If we have more than 0 elements, then
+  //      Iterate on each todo
+  //      create a new element
+  //      Add todo in it
+  //      Add delete button in it
+  //      Add data-index attribute with the todo index
+  //      append to container
+  // Focus in input
+  // Clear input
+};
+
+// On first load make sure to render UI from todos
+// Call render todos ui from local storage here
+
+// Add event listeners here
+// Add click event on "Add" button
+// Add click event on "Delete All" button
+// Add click event on todos container and make sure to check event.target for delete item is clicked
